420aeca21cc455a1f308e9896c354a6a57d4ff7b
antitta-collab <antitta@gmail.com>
2026-09-21 17:47:09 +0200

    Update sw.js

--- portafoglio-figlia/sw.js
+++ portafoglio-figlia/sw.js
@@ -1,4 +1,29 @@
-const CACHE='pf-figlia-v2';
-self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./manifest.json'])))});
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request))));
+const CACHE='pf-figlia-v3';
+const ASSETS=['./','./index.html','./manifest.json'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE).then(cache=>cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(k=>k.startsWith('pf-figlia-') && k!==CACHE)
+            .map(k=>caches.delete(k))
+        )
+      )
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  event.respondWith(
+    caches.match(event.request).then(hit=>hit||fetch(event.request))
+  );
+});
