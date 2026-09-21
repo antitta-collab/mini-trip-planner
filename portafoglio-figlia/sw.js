6c5c72cb440600aee1c363fa36a187d70ddb88b3
antitta-collab <antitta@gmail.com>
2026-09-21 17:28:51 +0200

    Add files via upload

--- portafoglio-figlia/sw.js
+++ portafoglio-figlia/sw.js
@@ -1,1 +1,4 @@
-const CACHE='pf-figlia-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='pf-figlia-v2';
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./manifest.json'])))});
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request))));
